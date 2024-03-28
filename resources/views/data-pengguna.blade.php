5a2f26b59d5e32c25c6f83c7b3267ac623a32b08
MuhSakaFathurrahman <msakafathurrahman@gmail.com>
2024-03-28 17:26:14 +0700

    data-pengguna & css nya

---
+++ resources/views/data-pengguna.blade.php
@@ -0,0 +1,168 @@
+<div class="data-pengguna">
+    <div class="navbar">
+      <div class="notification">
+        <div class="container-13">
+          <span class="4">
+            4
+          </span>
+        </div>
+        <img class="bell" />
+      </div>
+      <div class="container-17">
+        <div class="search-bar-10">
+          <img class="search-icon" />
+          <div class="search">
+            Search...
+          </div>
+        </div>
+        <div class="container-11">
+          <div class="username">
+            Username
+          </div>
+          <div class="photo-profile">
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="container-4">
+      <div class="data-pengguna-5">
+        Data Pengguna
+      </div>
+      <span class="data-pengguna-6">
+        Data Pengguna
+      </span>
+    </div>
+    <div class="content">
+      <div class="container-37">
+        <div class="ini-judul-konten">
+          Ini judul konten
+        </div>
+        <span class="ini-isi-konten">
+          Ini isi konten
+        </span>
+      </div>
+      <div class="line-7">
+      </div>
+    </div>
+    <div class="sidebar">
+      <div class="container-1">
+        <div class="line-4">
+        </div>
+        <div class="line-5">
+        </div>
+        <div class="line-6">
+        </div>
+      </div>
+      <div class="container-15">
+        <div class="sidebar-profile">
+          <div class="container-14">
+            <div class="photo-profile-8">
+            </div>
+            <div class="container-18">
+              <div class="hizrian">
+                Hizrian
+              </div>
+              <span class="administrator">
+                Administrator
+              </span>
+            </div>
+          </div>
+          <img class="polygon-1" />
+        </div>
+        <div class="menu-dashboard">
+          <img class="vector-1" />
+          <div class="dashboard">
+            Dashboard
+          </div>
+          <img class="segitiga-bawah-19" />
+        </div>
+        <div class="components">
+          COMPONENTS
+        </div>
+        <div class="data-pengguna-9">
+          <div class="container">
+            <div class="user-icon-24">
+              <div class="ellipse-922">
+              </div>
+              <img class="ellipse-1023" />
+            </div>
+            <div class="data-pengguna-21">
+              Data Pengguna
+            </div>
+          </div>
+          <img class="segitiga-bawah-20" />
+        </div>
+        <div class="data-kegiatan">
+          <div class="folder-image-streamline-ultimate-png">
+          </div>
+          <div class="data-kegiatan-26">
+            Data Kegiatan
+          </div>
+          <img class="segitiga-bawah-25" />
+        </div>
+        <div class="data-kesakitan">
+          <img class="group-237468" />
+          <div class="data-kesakitan-28">
+            Data Kesakitan
+          </div>
+          <img class="segitiga-bawah-27" />
+        </div>
+        <div class="data-gizi-kia">
+          <img class="gizi" />
+          <div class="data-gizi-kia-30">
+            Data Gizi &amp; KIA
+          </div>
+          <img class="segitiga-bawah-29" />
+        </div>
+        <div class="data-id-pkm">
+          <div class="folder-image-streamline-ultimate-png-32">
+          </div>
+          <div class="data-ppm">
+            Data PPM
+          </div>
+          <img class="segitiga-bawah-31" />
+        </div>
+        <div class="data-sp-2-tbulanan">
+          <div class="folder-image-streamline-ultimate-png-34">
+          </div>
+          <div class="container-3">
+            <span class="data-sp-2-tp-bulanan">
+              Data SP2TP Bulanan
+            </span>
+            <img class="segitiga-bawah-33" />
+          </div>
+        </div>
+        <div class="data-sp-2-ttahunan">
+          <div class="folder-image-streamline-ultimate-png-36">
+          </div>
+          <div class="container-16">
+            <span class="data-sp-2-tp-tahunan">
+              Data SP2TP Tahunan
+            </span>
+            <img class="segitiga-bawah-35" />
+          </div>
+        </div>
+      </div>
+      <div class="sipeter-logo">
+        <span class="s">
+          S
+        </span>
+        <div class="sipeter">
+          SIPETER
+        </div>
+      </div>
+      <div class="clickbar-main">
+        <div class="container-2">
+          <div class="user-icon">
+            <div class="ellipse-9">
+            </div>
+            <img class="ellipse-10" />
+          </div>
+          <div class="data-pengguna-12">
+            Data Pengguna
+          </div>
+        </div>
+        <img class="segitiga-bawah" />
+      </div>
+    </div>
+  </div>
