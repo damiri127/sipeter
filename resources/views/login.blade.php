999295368ef049936ed5e1071e2e06ffb02fdfc3
damiri127 <damiricepri127@gmail.com>
2024-04-04 08:40:03 +0700

    adding route login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,14 @@
-<div class="login">
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('layout_asset/assets/css/style.css') }}">
+  <title>Document</title>
+</head>
+<body>
+  <div class="login">
     <div class="container-5">
       <div class="container">
         <div class="ellipse-175">
@@ -62,3 +72,5 @@
       </div>
     </div>
   </div>
+</body>
+</html>
--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/get-user', function () {
+    dd('test api backend using laravel');
 });
